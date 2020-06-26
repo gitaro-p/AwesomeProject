de472864eb14cdd761e1149fb0807903e4011939
清川　裕太郎 <yuu7353@gmail.com>
2020-06-26 09:28:28 +0900

    更新

--- App.js
+++ App.js
@@ -1,19 +1,42 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './src/screens/Home';
+import Form from './src/screens/Form';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const Stack = createStackNavigator();
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      todoList: [],
+    };
+  }
+
+  addNewItem(todoList) {
+    this.setState({ todoList })
+  }
+
+  render() {
+    const { todoList } = this.state;
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            options={{ title: '一覧' }}
+          >
+            {props => <Home {...props} todoList={todoList} />}
+          </Stack.Screen>
+          <Stack.Screen
+            name="Form"
+            options={{ title: '登録' }}
+          >
+            {props => <Form {...props} todoList={todoList} addNewItem={v => this.addNewItem(v)} />}
+          </Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Home.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import {
+  SafeAreaView,
+  FlatList,
+  View,
+  Text,
+  StyleSheet,
+  Button
+} from 'react-native';
+
+export default class Home extends Component {
+  render() {
+    const { navigation, todoList } = this.props;
+
+    return (
+      <SafeAreaView style={styles.container}>
+        <Button
+          title="登録画面へ"
+          onPress={() => navigation.navigate('Form')}
+        />
+        <FlatList
+          style={styles.listBox}
+          data={todoList}
+          renderItem={({ item }) => {
+            return (
+              <View style={styles.listItem}>
+                <Text>{item.todo}</Text>
+                <Text>{item.memo}</Text>
+              </View>
+            );
+          }}
+          keyExtractor={item => item.todo}
+        />
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  listItem: {
+    height: 64,
+    width: 200,
+    marginTop: 16,
+    padding: 16,
+    borderColor: 'gray',
+    borderWidth: 1,
+  }
+});
